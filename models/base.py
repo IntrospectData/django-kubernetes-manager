9b21cbff64ffadb6a995196772792502ad56f50d
Bradley <bradley@introspectdata.com>
2020-02-18 19:19:46 -0800

    stubbing/tweakingmodels

--- models/base.py
+++ models/base.py
@@ -4,8 +4,9 @@ from kubernetes import client
 from kubernetes.client.rest import ApiException
 from django.db import models
 from .target_cluster import TargetCluster
+
 class KubernetesBase(model.Model):
-    id = UUIDField()
+    id = models.UUIDField(default=uuid4, editable=False)
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=128)
     namespace = models.CharField(max_length=128)
