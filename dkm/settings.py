339f06e9bb2e89b3ebcb16497e23eb4cd51f6235
Bradley <bradley@introspectdata.com>
2020-03-07 13:26:11 -0800

    drf set default perm class

--- dkm/settings.py
+++ dkm/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'PAGE_SIZE': 10
 }
 # Application definition
