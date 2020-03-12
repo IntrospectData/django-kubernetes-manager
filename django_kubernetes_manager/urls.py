07e04dc390ce729774579ae2e41086163117fd88
Bradley <bradley@introspectdata.com>
2020-03-12 15:37:21 -0700

    changed api base path

--- django_kubernetes_manager/urls.py
+++ django_kubernetes_manager/urls.py
@@ -34,9 +34,9 @@ router.register(r'clusters', views.TargetClusterViewSet)
 router.register(r'namespaces', views.KubernetesNamespaceViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api/auth/', include('rest_framework.urls', namespace='rest')),
-    url(r'^api/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('dkm/', include(router.urls)),
+    path('dkm/auth/', include('rest_framework.urls', namespace='rest')),
+    url(r'^dkm/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('dkm/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('dkm/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
