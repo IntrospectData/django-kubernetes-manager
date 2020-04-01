71fe74939e0bbb687b381113f046fbae45938f56
bradley <47170340+breimers@users.noreply.github.com>
2020-04-01 16:04:49 -0700

    Update admin.py

--- django_kubernetes_manager/admin.py
+++ django_kubernetes_manager/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django_kubernetes_manager.models import (TargetCluster,
     KubernetesContainer, KubernetesPodTemplate, KubernetesJob,
     KubernetesDeployment, KubernetesService, KubernetesIngress,
-    KubernetesNamespace, KubernetesConfigMap)
+    KubernetesNamespace, KubernetesConfigMap, KubernetesVolume, 
+    KubernetesVolumeMount)
 
 models = [TargetCluster,
     KubernetesContainer, KubernetesPodTemplate, KubernetesJob,
     KubernetesDeployment, KubernetesService, KubernetesIngress,
-    KubernetesNamespace, KubernetesConfigMap]
+    KubernetesNamespace, KubernetesConfigMap, KubernetesVolume, 
+    KubernetesVolumeMount]
 
 for model in models:
     admin.site.register(model)
